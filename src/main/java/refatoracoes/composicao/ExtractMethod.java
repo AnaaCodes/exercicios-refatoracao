6838be28041ee122d9b320cc697f1870b0a524e7
Analice <analicemendes.r@gmail.com>
2026-09-15 19:19:35 -0300

    Update ExtractMethod.java
    Refatoração

--- src/main/java/refatoracoes/composicao/ExtractMethod.java
+++ src/main/java/refatoracoes/composicao/ExtractMethod.java
@@ -1,20 +1,23 @@
-package refatoracoes.composicao;
-
 public class ExtractMethod {
-	void printOwing(String name) {
-		  printBanner();
 
-		  // Print details.
-		  System.out.println("name: " + name);
-		  System.out.println("amount: " + getOutstanding());
-		}
-	
-	private int getOutstanding() {
-		return 100;
-	}
+    public static void main(String[] args) {
+        printOwing("Maria");
+    }
 
-	private void printBanner() {
-		System.out.println("-- banner --");
-	}
+    static void printOwing(String name) {
+        printBanner();
+        printDetails(name, getOutstanding());
+    }
 
+    private static void printDetails(String name, double amount) {
+        System.out.printf("name: %s%namount: %.2f%n", name, amount);
+    }
+
+    private static void printBanner() {
+        System.out.println("-- banner --");
+    }
+
+    private static double getOutstanding() {
+        return 100.00;
+    }
 }
