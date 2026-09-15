c68b934cd831d8b23a9d9686c398c775866bd8d4
Analice <analicemendes.r@gmail.com>
2026-09-15 19:28:59 -0300

    Update PushDownMethod.java
    Especificação para evitar código morto sem sentido e com maior coesão

--- src/main/java/refatoracoes/generalizacao/PushDownMethod.java
+++ src/main/java/refatoracoes/generalizacao/PushDownMethod.java
@@ -1,10 +1,13 @@
 package refatoracoes.generalizacao;
 
+
+class SuperClass {
+    // Métodos gerais compartilhados por todas as subclasses ficam aqui
+}
+
 public class PushDownMethod extends SuperClass {
 
-	public void publicMethod() {
-		System.out.println("PushDownMethod");
-		
-	}
-
+    public void publicMethod() {
+        System.out.println("PushDownMethod");
+    }
 }
