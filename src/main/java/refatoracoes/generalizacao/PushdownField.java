d2c642932fab90674a5797b465226a6fe204afac
Analice <analicemendes.r@gmail.com>
2026-09-15 19:31:02 -0300

    Update PushdownField.java
    Refatoração - Evitar código poluído e melhor encapsulamento

--- src/main/java/refatoracoes/generalizacao/PushdownField.java
+++ src/main/java/refatoracoes/generalizacao/PushdownField.java
@@ -1,9 +1,24 @@
 package refatoracoes.generalizacao;
 
-public class PushdownField extends SuperClass{
+// A SuperClass fica limpa, sem o campo genérico que não pertencia a todas as subclasses..
+class SuperClass {
+    // Apenas atributos e métodos genuinamente comuns a todas as subclasses
+}
 
-	public void publicMethod() {
-		System.out.println("PushdownField");
-	}
+//O campo específico vão para as subclasse correta!
+public class PushdownField extends SuperClass {
 
+    private float health;
+
+    public void publicMethod() {
+        System.out.println("PushdownField");
+    }
+
+    public float getHealth() {
+        return this.health;
+    }
+
+    public void setHealth(float health) {
+        this.health = health;
+    }
 }
