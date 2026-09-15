b27c2135e22ce94c5be7717d6cc60b09aeab63f0
Analice <analicemendes.r@gmail.com>
2026-09-15 19:26:04 -0300

    Update PullUpField.java
    Retirar a duplicidade de informações e coesão do código

--- src/main/java/refatoracoes/generalizacao/PullUpField.java
+++ src/main/java/refatoracoes/generalizacao/PullUpField.java
@@ -1,15 +1,22 @@
 package refatoracoes.generalizacao;
 
+// 1. O campo e os acessores sobem para a SuperClass
+class SuperClass {
+    private float health;
+
+    public float getHealth() {
+        return this.health;
+    }
+
+    public void setHealth(float health) {
+        this.health = health;
+    }
+}
+
+// 2. A subclasse mantém apenas o comportamento específico dela
 public class PullUpField extends SuperClass {
-	
-	private float health;
 
-	public void publicMethod() {
-		System.out.println("PullUpField");	
-	}
-	
-	public float getHealth() {
-		return this.health;
-	}
-
+    public void publicMethod() {
+        System.out.println("PullUpField");
+    }
 }
